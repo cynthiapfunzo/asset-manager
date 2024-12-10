554ec1186c0f0559cff2706794b6251a85647aea
mofokeng-chaka <mofokeng.chk@gmail.com>
2024-12-10 14:45:02 +0200

    Remove file for clearing the database

--- clear_db.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import app, db
-
-with app.app_context():
-    db_file = 'assets.db'  # Your database file name
-    if os.path.exists(db_file):
-        os.remove(db_file)  # Remove the existing database file
-    db.create_all()  # Create a new database and tables
