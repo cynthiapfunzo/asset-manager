ebcd697fa5bcb7be0c6667439959ae3dee779425
mofokeng-chaka <mofokeng.chk@gmail.com>
2024-12-11 13:11:26 +0200

    Use custom table names

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    __tablename__ = "pfunzo_user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 class Asset(db.Model):
+    __tablename__ = "pfunzo_asset"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(50), nullable=False)
